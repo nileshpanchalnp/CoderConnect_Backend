8acd5285325d1d7970e0156ac4d23686346484df
nileshpanchalnp <nileshpanchal4411@gmail.com>
2025-11-28 11:46:00 +0530

    update

--- src/models/question.js
+++ src/models/question.js
@@ -7,14 +7,11 @@ const questionSchema = new mongoose.Schema(
     author_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     views: { type: Number, default: 0 },
     tags: [{ type: String, default: [] }],
-    // --- ADD THESE THREE LINES ---
-    // For Requirement 2: Total answer count
     answer_count: { type: Number, default: 0 }, 
-    // For Requirement 1: Total question votes
     vote_likes: { type: Number, default: 0 },
     vote_dislikes: { type: Number, default: 0 }
   },
-  { timestamps: true } // auto adds createdAt & updatedAt
+  { timestamps: true } 
 );
 
 export default mongoose.model("Question", questionSchema);
